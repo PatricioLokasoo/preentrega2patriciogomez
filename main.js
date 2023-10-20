19e63a5cf1ac6a7264c626652e6b598665b69159
PatricioLokasoo <140911863+PatricioLokasoo@users.noreply.github.com>
2023-10-20 01:56:05 -0300

    avansando con el estudio de los onjetos y arreglos y el foreach

--- main.js
+++ main.js
@@ -1,96 +1,107 @@
-/*  *
-  * Variables
- 
- let state = false
- let nombreUno = ""
- let rut = ""
- */
+// // variebles fijas
+// let state = false;
+// let Usuario = "";
+// let rut = "";
 
-/**
- * Users
- */
-/* let reservaUno = "juan soto";  
-let rutUno = 1888;                
-let reservaDos = "maria gomez";
-let rutDos = 1777;
-let reservaTres = "sofia duran";
-let rutTres = 1666;
-let reservaCuatro = "mario rojas";
-let rutCuatro = 1555;
+// function Usuarios(nombre, apellido, rut, mesa) {
+//   this.nombre = nombre;
+//   this.apellido = apellido;
+//   this.rut = rut;
+//   this.mesa = mesa;
+// }
 
-function bienvenida() {
-    alert("Buenas noches, agradeceriamos confirmar su reservación, ¿Nos puede indicar los siguientes datos?");
-}
+// const usuario1 = new Usuarios("juan", "soto", 1222, 1);
+// const usuario2 = new Usuarios("maria", "vega", 1333, 2);
+// const usuario3 = new Usuarios("mario", "veas", 1444, 3);
+// const usuario4 = new Usuarios("camila", "rojas", 1555, 4);
 
-function fraseFinal() {
-    alert("Gracias por venir");
-}
+// const usuarios = [usuario1, usuario2, usuario3, usuario4];
 
-function pedirDatos(){
-     nombreUno = prompt("Indiquenos su nombre y apellido").toLowerCase()
-     rut = Number(prompt("Indiquenos su RUT"))
-}
+// function bienvenida() {
+//   alert(
+//     "Buenas noches, agradeceriamos confirmar su reservación, ¿Nos puede indicar los siguientes datos?"
+//   );
+// }
+// bienvenida();
 
-function verificarUsuario(nom,rut) {
-    if (nom == reservaUno && rut == rutUno && nombreUno != "" && rut != "") {
-        alert(`${nom}, Hemos podido verificar tu nombre y rut, puedes ingresar.`)   
-        return true  
-    }else { alert("No te encuentras registrado lo sentimos, no puedes ingresar.")   
-        return false 
-    }
-}
+// function fraseFinal() {
+//   alert("Gracias por venir");
+// }
 
+// // Salida y peticion de datos al usuario para comprobar si esta registrado
+// function pedirDatos() {
+//   nombreUser = prompt("Indiquenos su nombre").toLowerCase();
+//   apellidoUser = prompt("Indiquenos su apellido").toLowerCase();
+//   rutUser = Number(prompt("Indiquenos su RUT"));
+//   mesaUser = Number(prompt("ingresa el numero de tu mesa"));
+// }
 
-    bienvenida();
-do {
-   pedirDatos()
-   state =  verificarUsuario(nombreUno,rut)
-} while (!state);
+// function verificarUsuario(nom, ape, rut, mes) {
+//   for (let i = 0; i < usuarios.length; i++) {
+//     if (
+//       nom == usuarios[i].nombre &&
+//       ape == usuarios[i].apellido &&
+//       rut == usuarios[i].rut &&
+//       mes == usuarios[i].mesa
+//     ) {
+//       alert(
+//         `${nombreUser}, Hemos podido verificar que tus datos son correctos, puedes ingresar.`
+//       );
+//       return true;
+//     }
+//   }
+//   alert("No te encuentras registrado, lo sentimos, no puedes ingresar.");
+//   return false;
+// }
 
-fraseFinal() */
+// do {
+//   pedirDatos();
+//   state = verificarUsuario(nombreUser, apellidoUser, rutUser, mesaUser);
+// } while (!state);
 
-// variebles fijas
+// function queDeseaComer() {
+//     alert("¿Qué deseas comer? Tenemos las siguientes opciones:");
+
+//     // Definir platos de comida y sus precios
+//     const platosComida = [
+//         new Platos("Sushi", 5000),
+//         new Platos("Pizza", 8000),
+//         // Agrega más platos aquí
+//     ];
+
+//     return platosComida;
+// }
+
+// function mostrarPlatos() {
+//     alert("Tenemos los siguientes platos disponibles:");
+
+//     const platos = queDeseaComer();
+
+//     platos.forEach(plato => {
+//         alert(`${plato.plato}: $${plato.precio}`);
+//     });
+// }
+
+// mostrarPlatos();
+
+// fraseFinal();
+
+// Variables fijas
 let state = false;
 let Usuario = "";
 let rut = "";
-
-// reemplace las variables sencillas por un array con objetos con 4 usuarios y sus atributos
-// const usuarios =[
-// {
-// nombre: "juan",
-// apellido: "soto",
-// rut: 1222,
-// mesa: 1,
-// },
-
-// {
-// nombre: "maria",
-// apellido: "vega",
-// rut: 1333,
-// mesa: 2,
-// },
-
-// {
-// nombre: "mario",
-// apellido: "veas",
-// rut: 1444,
-// mesa: 3,
-// },
-
-// {
-// nombre: "camila",
-// apellido: "rojas",
-// rut: 1555,
-// mesa: 4,
-// },
-
-// ];
 
 function Usuarios(nombre, apellido, rut, mesa) {
   this.nombre = nombre;
   this.apellido = apellido;
   this.rut = rut;
   this.mesa = mesa;
+}
+
+function Platos(plato, precio) {
+  // Define la clase Platos
+  this.plato = plato;
+  this.precio = precio;
 }
 
 const usuario1 = new Usuarios("juan", "soto", 1222, 1);
@@ -100,10 +111,9 @@ const usuario4 = new Usuarios("camila", "rojas", 1555, 4);
 
 const usuarios = [usuario1, usuario2, usuario3, usuario4];
 
-
 function bienvenida() {
   alert(
-    "Buenas noches, agradeceriamos confirmar su reservación, ¿Nos puede indicar los siguientes datos?"
+    `Buenas noches, ${Usuario}. Agradeceríamos confirmar su reservación. ¿Puede indicarnos los siguientes datos?`
   );
 }
 bienvenida();
@@ -112,12 +122,12 @@ function fraseFinal() {
   alert("Gracias por venir");
 }
 
-// Salida y peticion de datos al usuario para comprobar si esta registrado
+// Salida y petición de datos al usuario para comprobar si está registrado
 function pedirDatos() {
-  nombreUser = prompt("Indiquenos su nombre").toLowerCase();
-  apellidoUser = prompt("Indiquenos su apellido").toLowerCase();
-  rutUser = Number(prompt("Indiquenos su RUT"));
-  mesaUser = Number(prompt("ingresa el numero de tu mesa"));
+  Usuario = prompt("Indíquenos su nombre").toLowerCase(); // Asigna el nombre a Usuario
+  apellidoUser = prompt("Indíquenos su apellido").toLowerCase();
+  rutUser = Number(prompt("Indíquenos su RUT"));
+  mesaUser = Number(prompt("Ingrese el número de su mesa"));
 }
 
 function verificarUsuario(nom, ape, rut, mes) {
@@ -129,7 +139,7 @@ function verificarUsuario(nom, ape, rut, mes) {
       mes == usuarios[i].mesa
     ) {
       alert(
-        `${nombreUser}, Hemos podido verificar que tus datos son correctos, puedes ingresar.`
+        `${Usuario}, hemos podido verificar que tus datos son correctos. Puedes ingresar.`
       );
       return true;
     }
@@ -140,7 +150,64 @@ function verificarUsuario(nom, ape, rut, mes) {
 
 do {
   pedirDatos();
-  state = verificarUsuario(nombreUser, apellidoUser, rutUser, mesaUser);
+  state = verificarUsuario(Usuario, apellidoUser, rutUser, mesaUser);
 } while (!state);
+
+function queDeseaComer() {
+  alert("¿Qué deseas comer? Tenemos las siguientes opciones:");
+
+  // Definir platos de comida y sus precios
+  const platosComida = [
+    new Platos("Sushi", 6000),
+    new Platos("Espaguetis", 3000),
+    // Agrega más platos aquí
+  ];
+
+  return platosComida;
+}
+
+function mostrarPlatos() {
+  alert("Tenemos los siguientes platos disponibles:");
+
+  const platos = queDeseaComer();
+
+  platos.forEach((plato) => {
+    alert(`${plato.plato}: $${plato.precio}`);
+  });
+}
+
+mostrarPlatos();
+
+function seleccionarPlatos() {
+  const platos = queDeseaComer();
+  let totalCompra = 0;
+  const platosSeleccionados = [];
+
+  do {
+    const opcion = Number(
+      prompt(
+        "tenemos los siguientes platos\n1. Sushi: $6000\n2. Espaguetis: $3000"
+      )
+    );
+    if (opcion === 0) {
+      break;
+    } else if (opcion >= 1 && opcion <= platos.length) {
+      const platoElegido = platos[opcion - 1];
+      platosSeleccionados.push(platoElegido);
+      totalCompra += platoElegido.precio;
+      alert(`Has seleccionado: ${platoElegido.plato}`);
+    } else {
+      alert("Opción inválida. Por favor, elija un número de plato válido.");
+    }
+  } while (true);
+
+  alert(`Resumen de tu pedido:`);
+  platosSeleccionados.forEach((plato) => {
+    alert(`${plato.plato}: $${plato.precio}`);
+  });
+  alert(`Total de la compra: $${totalCompra}`);
+}
+
+seleccionarPlatos();
 
 fraseFinal();
